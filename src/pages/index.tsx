648b635cdaa76c1f0386fca7e81382bf69c72e70
Bruno Mariano <bsignx@gmail.com>
2020-08-28 16:49:13 -0300

    chore: initial setup

---
+++ src/pages/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Head from 'next/head'
+
+const Home: React.FC = () => {
+  return (
+    <div>
+      <Head>
+        <title>Loan.prototype - Plataforma de empréstimos</title>
+      </Head>
+
+      <h1>Olá</h1>
+    </div>
+  )
+}
+
+export default Home
